29807f7b143c40b3f3c5e4500bffe48388af062e
shengkai kwan <skkwan97@gmail.com>
2022-08-20 13:31:38 +0800

    update release

--- ms-inventory-core/src/main/java/ms/inventory/core/exception/InventoryError.java
+++ ms-inventory-core/src/main/java/ms/inventory/core/exception/InventoryError.java
@@ -6,10 +6,7 @@ public enum InventoryError implements BaseError {
     UNIT_MEASUREMENT_ENUM_VALUE_NOT_FOUND("10001", "Unit Measurement Enum Value Not Found"),
     STOCK_RECORD_ALREADY_EXIST("11001", "Stock Record Already Exist"),
     STOCK_RECORD_NOT_FOUND("11002", "Stock Record Not Found"),
-    STOCK_ID_NULL("11003", "Stock Id Is Required"),
-    PRODUCT_RECORD_ALREADY_EXIST("12001", "Product Record Already Exist"),
-    PRODUCT_RECORD_NOT_FOUND("12002", "Product Record Not Found"),
-    PRODUCT_ID_NULL("12003", "Product Id Is Required");
+    STOCK_ID_NULL("11003", "Stock Id Is Required");
 
     private String application = "MS-INVENTORY";
     private String code;
